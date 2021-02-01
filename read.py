67d950b535927dd1819271fd91acdf02b11ff313
AmanRawat130ss <78321530+AmanRawat130ss@users.noreply.github.com>
2021-02-01 20:46:46 +0530

    Add files via upload

---
+++ read.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+
+# img = cv.imread('Memes/peter.jpg')
+
+# cv.imshow('SpiderMan',img)
+
+cap = cv.VideoCapture('video/b.mp4')
+while True:
+    frame = cap.read()
+   
+    cv.imshow('video',frame)
+
+    if cv.waitKey(5) & 0xFF==ord('d'):
+        break
+cap.release()
+cv.destroyAllWindows
+cv.waitKey(0)
